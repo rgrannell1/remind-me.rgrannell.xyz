185943f4122656523d725521fdab3bba72ed6d2d
Róisín Grannell <r.grannell2@gmail.com>
2025-01-12 21:33:57 +0000

    tidied js

--- index.js
+++ index.js
@@ -16,53 +16,58 @@ async function checkPermissions() {
   }
 }
 
-/* */
-document.getElementById("reminder-form").addEventListener(
-  "submit",
-  async (event) => {
-    event.preventDefault();
-    await checkPermissions();
+async function onFormSubmit(event) {
+  event.preventDefault();
+  await checkPermissions();
 
-    const timeInput = document.getElementById("time").value;
-    const message = document.getElementById("message").value;
+  const timeInput = document.getElementById("time").value;
+  const message = document.getElementById("message").value;
 
-    const minutes = parseInt(timeInput, 10);
-    if (isNaN(minutes) || minutes <= 0) {
-      alert("Please enter a valid time.");
-      return;
-    }
+  const minutes = parseInt(timeInput, 10);
+  if (isNaN(minutes) || minutes <= 0) {
+    alert("Please enter a valid time.");
+    return;
+  }
 
-    if (!navigator.serviceWorker.controller) {
-      alert("Service Worker not ready. Try again later.");
-      return;
-    }
+  if (!navigator.serviceWorker.controller) {
+    alert("Service Worker not ready. Try again later.");
+    return;
+  }
 
-    console.log("Sending message to service worker...", {
-      type: "set-reminder",
-      time: minutes,
-      message: message,
-    });
-    navigator.serviceWorker.controller.postMessage({
-      type: "set-reminder",
-      id: `reminder-${Date.now()}`,
-      time: minutes,
-      message: message,
-    });
-  },
-);
+  console.log("Sending message to service worker...", {
+    type: "set-reminder",
+    time: minutes,
+    message: message,
+  });
+  navigator.serviceWorker.controller.postMessage({
+    type: "set-reminder",
+    id: `reminder-${Date.now()}`,
+    time: minutes,
+    message: message,
+  });
+}
 
+/*
+ * Respond to a service-worker message with a progress update
+ *
+ */
 function onServiceWorkerMessage(event) {
   console.log("Message received from service worker: ", event.data);
   const { type, remainingMinutes } = event.data;
 
   if (type === "update-reminder") {
-    document.title = `Time left: ${remainingMinutes}m`;
-  }
-
-  if (type === "reminder-complete") {
+    document.title = `t-${remainingMinutes}m`;
+  } else if (type === "reminder-complete") {
     document.title = "Reminder Complete!";
   }
 }
 
-/* */
+/*
+ * On form submit, start a reminder
+ */
+document.getElementById("reminder-form").addEventListener(
+  "submit",
+  onFormSubmit,
+);
+
 navigator.serviceWorker.addEventListener("message", onServiceWorkerMessage);
